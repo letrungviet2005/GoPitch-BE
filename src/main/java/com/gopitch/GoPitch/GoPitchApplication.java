45e1379326c4e476a75d629e4fbc7d7ceaa46a27
letrungviet2005 <vitprofb@gmail.com>
2025-07-23 12:00:44 +0700

    new upload

--- src/main/java/com/gopitch/GoPitch/GoPitchApplication.java
+++ src/main/java/com/gopitch/GoPitch/GoPitchApplication.java
@@ -1,13 +1,13 @@
-package com.example.GoPitch;
+package com.gopitch.GoPitch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class GoPitchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(GoPitchApplication.class, args);
 	}
 
 }
