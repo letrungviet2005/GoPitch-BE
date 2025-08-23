165d66e5ffbcc254d27d0bb0a8511543e9cc93c9
letrungviet2005 <vitprofb@gmail.com>
2025-08-23 17:35:17 +0700

    feat(ui): new uploaD

--- src/main/java/com/gopitch/GoPitch/GoPitchApplication.java
+++ src/main/java/com/gopitch/GoPitch/GoPitchApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GoPitchApplication {
 
-	// public static void main(String[] args) {
-	// 	SpringApplication.run(GoPitchApplication.class, args);
-	// }
+	public static void main(String[] args) {
+		SpringApplication.run(GoPitchApplication.class, args);
+	}
 
 }
