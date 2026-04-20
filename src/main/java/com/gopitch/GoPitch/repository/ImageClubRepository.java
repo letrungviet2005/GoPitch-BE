7897cb8b58d795b714a17a073fdc108920243936
letrungviet2005 <vitprofb@gmail.com>
2026-04-20 18:40:06 +0700

    feat(be): new upload

--- src/main/java/com/gopitch/GoPitch/repository/ImageClubRepository.java
+++ src/main/java/com/gopitch/GoPitch/repository/ImageClubRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ImageClubRepository extends JpaRepository<ImageClub, Long> {
-    // Additional query methods can be defined here if needed
     boolean existsById(long id);
 
     boolean existsByImageUrlAndIdNot(String imageUrl, Long id);
